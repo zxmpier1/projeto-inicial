9ed3069c1de6e125bb5c64553bf686a1ed92070a
zxmpieri <zxmpieri16@gmail.com>
2025-09-30 17:23:50 -0300

    at3

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
     function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-    responsiveVoice.speak(texto, 'Brazilian Portuguese Male', {rate:1.2});
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Male', {rate:1.5});
 }
 
     function exibirMensagemInicial() {
